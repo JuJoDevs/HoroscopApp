6bb3677267cb47b3e1980548f6b1c5ad262799e0
jujob <jujo.bonillo90@gmail.com>
2023-09-05 23:05:25 +0200

    Add toolbar

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+https://github.com/ArisGuimera/Android-Expert-Intermedio
--- app/src/main/java/com/jujodevs/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/jujodevs/horoscapp/ui/home/MainActivity.kt
@@ -2,15 +2,31 @@ package com.jujodevs.horoscapp.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jujodevs.horoscapp.R
 import com.jujodevs.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUi()
+    }
+
+    private fun initUi() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
