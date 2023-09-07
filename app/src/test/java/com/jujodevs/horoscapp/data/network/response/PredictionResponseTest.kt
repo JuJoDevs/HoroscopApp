1337b9c7998886e92e1bc7a04f51eaf5af6f53b6
jujob <jujo.bonillo90@gmail.com>
2023-09-07 21:26:58 +0200

    unit testing

---
+++ app/src/test/java/com/jujodevs/horoscapp/data/network/response/PredictionResponseTest.kt
@@ -0,0 +1,23 @@
+package com.jujodevs.horoscapp.data.network.response
+
+import com.jujodevs.horoscapp.motherobject.HoroscopeMotherObject.anyResponse
+import io.kotlintest.shouldBe
+import org.junit.Test
+
+class PredictionResponseTest{
+
+    @Test
+    fun `toDomain should return a correct PredictionModel`(){
+        //Given
+        val horoscopeResponse = anyResponse.copy(sign = "libra")
+
+        //When
+        val predictionModel = horoscopeResponse.toDomain()
+
+        //Then
+        predictionModel.sign shouldBe horoscopeResponse.sign
+        predictionModel.horoscope shouldBe horoscopeResponse.horoscope
+
+    }
+
+}
